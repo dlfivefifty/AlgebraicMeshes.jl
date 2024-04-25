9576da9852288ca5bd6a269645cb7eb7c4e9b153
Sheehan Olver <solver@mac.com>
2024-04-25 13:51:44 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,15 @@ using AlgebraicMeshes, DomainSets, StaticArrays, Test
 using DomainSets: ×
 
 
-rects = [(0..1) × (1..2) , (0..1) × (0..1), (1..2) × (0..1)]
+@testset "basic tests" begin
+    rects = [(0..1) × (1..2) , (0..1) × (0..1), (1..2) × (0..1)]
 
-mesh = AlgebraicMesh(rects)
+    mesh = AlgebraicMesh(rects)
 
-@test SVector(0.1,0.2) ∈ mesh
-@test SVector(1.1,0.2) ∈ mesh
-@test SVector(0.1,1.2) ∈ mesh
-@test SVector(1.1,1.2) ∉ mesh
+    @test SVector(0.1,0.2) ∈ mesh
+    @test SVector(1.1,0.2) ∈ mesh
+    @test SVector(0.1,1.2) ∈ mesh
+    @test SVector(1.1,1.2) ∉ mesh
+end
+
+include("test_makieext.jl")
